f3a522fcdc8827320b1480d99b8c52601e9b7c1e
Hasnain Ali <hasnain7815@gmail.com>
2023-10-27 00:01:41 +0100

    Basic navbar created for all pages

---
+++ used-book-selling-site/resources/views/layouts/base.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Used book selling</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-start">
+                <a class="navbar-brand text-white" href="{{ route('home') }}">Books4less</a>
+            </div>
+            <div class="d-flex justify-content-end align-items-center">
+                @auth
+                    <a class="nav-link text-white" href="">{{ auth()->user()->name }}</a> 
+
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" style="background: none; border: none; color: white; cursor: pointer;">Logout</button>
+                    </form>
+                    
+                @endauth
+
+                @guest
+                    <a class="nav-link text-white" href="{{ route('register') }}">Register</a>
+                    <a class="nav-link text-white" href="{{ route('login') }}">Login</a>
+                @endguest
+                
+            </div>
+        </div>
+    </nav>
+
+    
+    @yield('content')
+
+</body>
+</html>
